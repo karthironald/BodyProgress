889e773431180ec8406992ce89339f3d4d39b533
Karthick Selvaraj <karthick@mallow-tech.com>
2020-07-17 23:42:05 +0530

    Have fixed body part summary issue.

--- BodyProgress/Views/WorkoutHistory/BodyPartSummaryRow.swift
+++ BodyProgress/Views/WorkoutHistory/BodyPartSummaryRow.swift
@@ -23,50 +23,50 @@ struct BodyPartSummaryRow: View {
     
     var body: some View {
         GeometryReader { geo in
-            ZStack {
-                HStack {
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("\(self.summary.workout)")
-                            .font(kPrimaryBodyFont)
-                            .bold()
-                        VStack(alignment: .leading, spacing: 10) {
-                            HStack(spacing: 10) {
-                                ZStack(alignment: .trailing) {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .fill(self.appSettings.themeColorView())
-                                        .frame(width: (geo.size.width / 100) * CGFloat(self.percentage), height: 20)
+            VStack(alignment: .leading, spacing: 5) {
+                Text("\(self.summary.workout)")
+                    .font(kPrimaryBodyFont)
+                    .bold()
+                VStack(alignment: .leading, spacing: 10) {
+                    HStack(spacing: 10) {
+                        ZStack(alignment: .leading) {
+                            RoundedRectangle(cornerRadius: 12.5)
+                                .fill(Color.black.opacity(0.1))
+                                .frame(width: geo.size.width, height: 25)
+                            RoundedRectangle(cornerRadius: 12.5)
+                                .fill(self.appSettings.themeColorView())
+                                .frame(width: (geo.size.width / 100) * CGFloat(self.percentage), height: 25)
+                                .overlay(
                                     Text("\(self.percentage, specifier: "%0.2f") %")
-                                        .font(kPrimaryFootnoteFont)
-                                        .foregroundColor(.white)
-                                        .padding([.leading, .trailing], 5)
-                                }
+                                    .font(kPrimaryFootnoteFont)
+                                    .foregroundColor(.white)
+                                    .padding([.leading, .trailing], 5)
+                                    , alignment: .trailing)
+                        }
+                    }
+                    if self.shouldShowDetails {
+                        Group {
+                            HStack {
+                                Text("Total: \(self.summary.sum.detailedDisplayDuration())")
+                                    .modifier(SummaryModifier(geo: geo))
                                 Spacer()
+                                Text("Sessions: \(Int(self.summary.count))")
+                                    .modifier(SummaryModifier(geo: geo))
                             }
-                            if self.shouldShowDetails {
-                                Group {
-                                    HStack {
-                                        Text("Total: \(self.summary.sum.detailedDisplayDuration())")
-                                            .modifier(SummaryModifier(geo: geo))
-                                        Spacer()
-                                        Text("Sessions: \(Int(self.summary.count))")
-                                            .modifier(SummaryModifier(geo: geo))
-                                    }
-                                    HStack {
-                                        Text("Min: \(self.summary.min.detailedDisplayDuration())")
-                                            .modifier(SummaryModifier(geo: geo))
-                                        Spacer()
-                                        Text("Max: \(self.summary.max.detailedDisplayDuration())")
-                                            .modifier(SummaryModifier(geo: geo))
-                                    }
-                                    HStack {
-                                        Text("Avg. session duration: \(self.summary.average.detailedDisplayDuration())")
-                                            .frame(width: geo.size.width, height: 25)
-                                            .font(kPrimaryFootnoteFont)
-                                            .background(Color.black.opacity(0.1))
-                                            .foregroundColor(Color.black.opacity(0.7))
-                                            .clipShape(RoundedRectangle(cornerRadius: 15))
-                                    }
-                                }
+                            HStack {
+                                Text("Min: \(self.summary.min.detailedDisplayDuration())")
+                                    .modifier(SummaryModifier(geo: geo))
+                                Spacer()
+                                Text("Max: \(self.summary.max.detailedDisplayDuration())")
+                                    .modifier(SummaryModifier(geo: geo))
+                            }
+                            HStack {
+                                Text("Avg. session duration: \(self.summary.average.detailedDisplayDuration())")
+                                    .frame(width: geo.size.width, height: 25)
+                                    .font(kPrimaryFootnoteFont)
+                                    .background(Color.black.opacity(0.1))
+                                    .foregroundColor(Color.black.opacity(0.7))
+                                    .clipShape(RoundedRectangle(cornerRadius: 15))
                             }
                         }
                     }
@@ -79,7 +79,7 @@ struct BodyPartSummaryRow: View {
 
 struct BodyPartSummaryRow_Previews: PreviewProvider {
     static var previews: some View {
-        BodyPartSummaryRow(summary: (3600, 120, 3480, 360, 10, "Biceps"), bodyPart: .arms, total: 7200, shouldShowDetails: false)
+        BodyPartSummaryRow(summary: (3600, 120, 3480, 360, 10, "Biceps"), bodyPart: .arms, total: 7200, shouldShowDetails: false).environmentObject(AppSettings())
     }
 }
 
