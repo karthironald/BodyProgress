1e5bf24b9a08d493c4a563f4b9fc6483c116a09c
Karthick Selvaraj <karthick@mallow-tech.com>
2020-07-24 23:59:52 +0530

    Google analytics added.

--- BodyProgress/Supporting Files/AppDelegate.swift
+++ BodyProgress/Supporting Files/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreData
 import UserNotifications
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
     }
     
